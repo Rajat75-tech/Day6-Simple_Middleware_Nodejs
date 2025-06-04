fff9ef5f8ee1429cdfbcc4e456190c8fd5a0738d
Rajat75-tech <rssharma8080957@gmail.com>
2025-06-04 21:37:55 +0530

    success

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 const express = require("express");
-const logger = require("logger");
+const logger = require("./logger");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -10,6 +10,8 @@ app.use(logger);
 app.get("/", (req, res) => {
   res.send("Hello from Express!");
 });
+
+app.listen(3000, () => console.log('Server running on port 3000'));
 
 app.get("/about", (req, res) => {
   res.send("This is the About page.");
